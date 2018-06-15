33d2cbfdf39db22b54776d6978833d0f1fc0018f
Irina Zhuravleva <sparrow113@rambler.ru>
2018-06-15 08:48:17 +0300

    Диалоговое окно и цвета персонажа

--- js/setup.js
+++ js/setup.js
@@ -51,3 +51,102 @@ for (var i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+// ?
+
+(function () {
+
+var userDialog = document.querySelector('.setup');
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  window.util.isEscEvent(evt, closePopup);
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var userNameInput = setup.querySelector('.setup-user-name');
+userNameInput.addEventListener ('invalid', function (evt) {
+  if (!userNameInput.validity.valid) {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    }
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  window.util.isEnterEvent(evt, openPopup);
+});
+
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  window.util.isEnterEvent(evt, closePopup);
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+var wizardCoatPopup = document.querySelector('.wizard-coat');
+var wizardEyesPopup = document.querySelector('.wizard-eyes');
+var fireballPopup = document.querySelector('.setup-fireball-wrap');
+
+window.colorize(wizardCoatPopup, function (color) {
+    wizardCoatPopup.style.fill = color;
+  });
+window.colorize(wizardEyesPopup, function (color) {
+    wizardEyesPopup.style.backgroundColor = color;
+  });
+window.colorize(fireballPopup, function (color) {
+    fireball.style.backgroundColor = color;
+  });
+
+})();
+
+
+(function () {
+  var COLORS = ['red', 'green', 'blue'];
+
+  var getRandomColor = function () {
+    return COLORS[Math.floor(COLORS.length * Math.random())];
+  };
+
+  window.colorize = function (element) {
+    element.addEventListener('click', function () {
+      var color = getRandomColor();
+      if (element.tagName.toLowerCase() === 'div') {
+        element.style.backgroundColor = color;
+      } else {
+        element.style.fill = color;
+      }
+    })
+  }
+})()
